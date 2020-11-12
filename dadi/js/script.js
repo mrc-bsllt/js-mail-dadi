d0f438ca0fcd32f9eabc09ef9937adc5b6a9fcba
MarcoB <mrc.bsllt@gmail.com>
2020-11-12 14:15:21 +0100

    completato esercizio dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,28 +3,31 @@
 // giocatore sia per il compute. Stabilire il
 // vincitore, in base a chi fa il punteggio più alto.
 
-
-
-
 function myFunction() {
 
   // genero un numero casuale del computer
   var randonNumberComputer = Math.floor((Math.random() * 6) + 1);
-  console.log(randonNumberComputer);
+  document.getElementById('pc-number').innerHTML = randonNumberComputer;
 
   // genero un numero casuale dell' utente
   var randonNumberUser = Math.floor((Math.random() * 6) + 1);
-  console.log(randonNumberUser);
+  document.getElementById('user-number').innerHTML = randonNumberUser;
+
+  var messaggio = "";
 
   if (randonNumberComputer > randonNumberUser) {
     // cosa succede quando il numero del computer è più alto di quello utente
-    console.log("Hai Perso!");
+    messaggio = "Hai Perso!";
+    document.getElementById('result').innerHTML = messaggio;
   } else if (randonNumberComputer < randonNumberUser) {
     // cosa succede quando il numero del computer è più basso di quello utente
-    console.log("Hai Vinto!");
+    messaggio = "Hai Vinto!";
+    document.getElementById('result').innerHTML = messaggio;
   } else {
     // cosa succede quando il numero del computer è uguale a quello utente
-    console.log("Pari, ritira il dado!");
+    messaggio = "Pari, ritira il dado!";
+    document.getElementById('result').innerHTML = messaggio;
+
   }
-  
+
 }
