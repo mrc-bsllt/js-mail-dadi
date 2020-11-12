f9dd41d3eab7721e638e96de14faabadfb044dbf
MarcoB <mrc.bsllt@gmail.com>
2020-11-12 14:26:30 +0100

    creato promp e arrey della mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,11 @@
-console.log("ciao prova");
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// chiedo la mail all'utente
+var personalEmail = prompt("Scrivi la tua Email");
+console.log(personalEmail);
+
+// creo una lista di email valide
+var emailList = ["marco.busellato@libero.it", "giuseppe.busellato@libero.it", "pietro.busellato@libero.it", "matteo.busellato@libero.it", "mattia.busellato@libero.it", "piero.busellato@libero.it", "stefano.busellato@libero.it", "simone.busellato@libero.it", "alessandro.busellato@libero.it", "mario.busellato@libero.it",];
+console.log(emailList);
