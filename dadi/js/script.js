7858efc3e5bec598e480cf3327b440242386989b
MarcoB <mrc.bsllt@gmail.com>
2020-11-12 13:41:39 +0100

    aggiunto css e bottone nell'html

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,21 +3,28 @@
 // giocatore sia per il compute. Stabilire il
 // vincitore, in base a chi fa il punteggio più alto.
 
-// genero un numero casuale del computer
-var randonNumberComputer = Math.floor((Math.random() * 6) + 1);
-console.log(randonNumberComputer);
 
-// genero un numero casuale dell' utente
-var randonNumberUser = Math.floor((Math.random() * 6) + 1);
-console.log(randonNumberUser);
 
-if (randonNumberComputer > randonNumberUser) {
-  // cosa succede quando il numero del computer è più alto di quello utente
-  console.log("Hai Perso!");
-} else if (randonNumberComputer < randonNumberUser) {
-  // cosa succede quando il numero del computer è più basso di quello utente
-  console.log("Hai Vinto!");
-} else {
-  // cosa succede quando il numero del computer è uguale a quello utente
-  console.log("Pari, ritira il dado!");
+
+function myFunction() {
+
+  // genero un numero casuale del computer
+  var randonNumberComputer = Math.floor((Math.random() * 6) + 1);
+  console.log(randonNumberComputer);
+
+  // genero un numero casuale dell' utente
+  var randonNumberUser = Math.floor((Math.random() * 6) + 1);
+  console.log(randonNumberUser);
+
+  if (randonNumberComputer > randonNumberUser) {
+    // cosa succede quando il numero del computer è più alto di quello utente
+    console.log("Hai Perso!");
+  } else if (randonNumberComputer < randonNumberUser) {
+    // cosa succede quando il numero del computer è più basso di quello utente
+    console.log("Hai Vinto!");
+  } else {
+    // cosa succede quando il numero del computer è uguale a quello utente
+    console.log("Pari, ritira il dado!");
+  }
+  
 }
